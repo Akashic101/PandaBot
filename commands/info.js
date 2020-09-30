a15bede5b66490c2e21f83b8ef839bedcc341b9b
David Moll <david.leander.moll@gmail.com>
2020-09-30 18:25:15 +0200

    Removed log

--- commands/info.js
+++ commands/info.js
@@ -5,30 +5,8 @@ const fs = require('fs');
 module.exports = {
     name: 'info',
     description: 'Sends info about the bot',
+    color: '#66FFFF',
     execute(client, message, args) {
-
-        var date = new Date();
-
-        const helpEmbed = new Discord.MessageEmbed()
-            .setColor('#a16d31')
-            .setTitle(`**info**`)
-            .addFields({
-                name: 'Username',
-                value: message.member.user.tag
-            }, {
-                name: 'Command',
-                value: message.content
-            }, {
-                name: 'Date',
-                value: date
-            })
-            .setThumbnail(message.member.user.displayAvatarURL({
-                format: 'jpg'
-            }))
-            .setTimestamp()
-            .setFooter(`${process.env.BOT_NAME} V ${pjson.version}`, process.env.BOT_PFP);
-        const channel = message.client.channels.cache.get(process.env.SERVER_LOG_CHANNEL);
-        channel.send(helpEmbed);
 
         fs.readdir('./commands', (err, files) => {
             let infoEmbed = new Discord.MessageEmbed()
