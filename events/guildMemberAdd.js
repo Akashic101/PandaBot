51918712a551d6e0c43e7b63125883ba85bfc9c3
David Moll <david.leander.moll@gmail.com>
2021-05-19 19:45:52 +0200

    Added auto-role

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -29,6 +29,10 @@ module.exports = async (client, member) => {
         .setFooter(`${process.env.BOT_NAME} V ${pjson.version}`, process.env.BOT_PFP);
     client.channels.cache.get(process.env.SERVER_LOG_CHANNEL).send(memberJoinedEmbed);
 
+    let role = member.guild.roles.cache.find(role => role.id == "720763494139428994")
+
+    member.roles.add(role)
+
     var item = lines[Math.floor(Math.random() * lines.length)];
     return client.channels.cache.get("758341976369397761").send(`Hey <@${member.user.id}>. Welcome to The Cupboard! Please read and agree to the <#750797155928899636> in order to post in our server! Feel free to tell us a bit about yourself in <#758341976369397761>? ${item}`);
 };
